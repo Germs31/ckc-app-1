abfbbe1671d206d3ba75596d5f8cfe8b0bf10257
German Nunez <german90nunez@gmail.com>
2020-01-30 22:05:46 -0800

    index.vue updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,119 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
+  <div>
+    <v-row>
+      <v-col md="4">
+        <h2 class="text-center mb-5">Recently Read Books</h2>
+        <v-row v-for="(item,index) in recentBooks" :key="index">
+          <BookCard
+            class="mb-5"
+            :bookTitle="item.title"
+            :bookAuthor="item.author"
+            :bookDescription="item.description"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
+            <template v-slot:button>
+              <v-btn @click.stop="edit(item,index)">Edit</v-btn>
+              <v-btn @click.stop="remove(item.category, index)">Remove</v-btn>
+            </template>
+          </BookCard>
+        </v-row>
+      </v-col>
+      <v-col md="4">
+        <h2 class="text-center mb-5">Favourite Books</h2>
+        <v-row v-for="(item,index) in favouriteBooks" :key="index">
+          <BookCard
+            class="mb-5"
+            :bookTitle="item.title"
+            :bookAuthor="item.author"
+            :bookDescription="item.description"
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            <template v-slot:button>
+              <v-btn @click.stop="edit(item,index)">Edit</v-btn>
+              <v-btn @click.stop="remove(item.category, index)">Remove</v-btn>
+            </template>
+          </BookCard>
+        </v-row>
+      </v-col>
+      <v-col md="4">
+        <h2 class="text-center mb-5">Best of the Best</h2>
+        <v-row v-for="(item,index) in bestOfTheBest" :key="index">
+          <BookCard
+            class="mb-5"
+            :bookTitle="item.title"
+            :bookAuthor="item.author"
+            :bookDescription="item.description"
           >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+            <template v-slot:button>
+              <v-btn @click.stop="edit(item,index)">Edit</v-btn>
+              <v-btn @click.stop="remove(item.category, index)">Remove</v-btn>
+            </template>
+          </BookCard>
+        </v-row>
+      </v-col>
+    </v-row>
+    <BookModal />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import BookCard from "@/components/BookCard";
+import BookModal from "@/components/BookModal";
+import { eventBus } from "@/eventBus";
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    BookCard,
+    BookModal
+  },
+  data() {
+    return {
+      recentBooks: [],
+      favouriteBooks: [],
+      bestOfTheBest: []
+    };
+  },
+  created() {
+    eventBus.$on("save-book", cardData => {
+      if (cardData.category === "Recently read books") {
+        this.recentBooks.push(cardData);
+        this.recentBooks.sort((a, b) => b - a);
+      }
+      if (cardData.category === "Favourite books") {
+        this.favouriteBooks.push(cardData);
+        this.favouriteBooks.sort((a, b) => b - a);
+      }
+      if (cardData.category === "Best of the best") {
+        this.bestOfTheBest.push(cardData);
+        this.bestOfTheBest.sort((a, b) => b - a);
+      }
+    });
+  },
+  methods: {
+    remove(category, index) {
+      if (category === "Recently read books") {
+        this.recentBooks.splice(index, 1);
+      }
+      if (category === "Favourite books") {
+        this.favouriteBooks.splice(index, 1);
+      }
+      if (category === "Best of the best") {
+        this.bestOfTheBest.splice(index, 1);
+      }
+    },
+    edit(item, index) {
+      if (item.category === "Recently read books") {
+        eventBus.$emit("open-add-book-modal", item);
+        this.recentBooks.splice(index, 1);
+      }
+      if (item.category === "Favourite books") {
+        eventBus.$emit("open-add-book-modal", item);
+        this.favouriteBooks.splice(index, 1);
+      }
+      if (item.category === "Best of the best") {
+        eventBus.$emit("open-add-book-modal", item);
+        this.bestOfTheBest.splice(index, 1);
+      }
+    }
   }
-}
+};
 </script>
+<style lang="scss" scoped>
+</style>
